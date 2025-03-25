329bec262e47ce119c9d30eb0706e9c2db68d7d1
chanwoo <66253711+chanwoo000@users.noreply.github.com>
2025-03-25 19:59:11 +0900

    refactor : 로그인 회원가입 페이지 분리

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,13 +4,14 @@ import LoginPage from "./pages/LoginPage";
 import LandingPage from "./pages/LandingPage";
 import CreateVoice from "./pages/CreateVoice";
 import VoiceMarket from "./pages/VoiceMarket";
+import SignupPage from "./pages/SignupPage"
 
 function App() {
   return (
       <Router>
         <Routes>
             <Route path="/login" element={<LoginPage />} />
-            <Route path="/signup" element={<LoginPage />} />
+            <Route path="/signup" element={<SignupPage/>}/>
             <Route path="/landingpage" element={<LandingPage />} />
             <Route path="/createvoice" element={<CreateVoice />} />
             <Route path="/voicemarket" element={<VoiceMarket />} />
