ddd5987a7b2afc9373607c4baaf892bf6df01b92
chanwoo <66253711+chanwoo000@users.noreply.github.com>
2025-04-16 10:13:47 +0900

    refactor: 문장 수정

--- frontend/src/pages/BasicVoice.js
+++ frontend/src/pages/BasicVoice.js
@@ -88,7 +88,7 @@ const BasicVoice = () => {
             보이스 변환 중...
           </p>
           <p className="mt-4 text-indigo-500 font-semibold text-lg animate-pulse">
-            "페이지를 벗어나면 보이스 변환이 취소될 수 있습니다."
+            페이지를 벗어나면 보이스 변환이 취소될 수 있어요!
           </p>
         </div>
       )}
