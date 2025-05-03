8c04f8fd8c0776ab8f1d2146a422d7c5fbb61aec
suwith <suwith@kookmin.ac.kr>
2025-05-03 21:29:49 +0900

    feat: 로그인없이 접근시 로그인 페이지로 리다이렉트 #97

--- frontend/src/pages/mypage/index.js
+++ frontend/src/pages/mypage/index.js
@@ -1,15 +1,24 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import MyDashboard from './MyDashboard';
 import MyVoicepacks from './MyVoicepacks';
 import MyRevenue from './MyRevenue';
 import MyPayments from './MyPayments';
 import useUserStore from '../../utils/userStore';
 import useVoicepackUsage from '../../hooks/useVoicepackUsage';
+import { useNavigate } from 'react-router-dom';
 
 const MyPage = () => {
   const [tab, setTab] = useState('dashboard');
+  const navigate = useNavigate();
 
   const user = useUserStore((state) => state.user);
+
+  // 로그인 안했다면 자동 리다이렉트
+  useEffect(() => {
+    if (!user) {
+      navigate('/sign-in');
+    }
+  }, [user, navigate]);
 
   const { voicepacks: createdVoicepacks } = useVoicepackUsage('mine');
   const { voicepacks: boughtVoicepacks } = useVoicepackUsage('purchased');
