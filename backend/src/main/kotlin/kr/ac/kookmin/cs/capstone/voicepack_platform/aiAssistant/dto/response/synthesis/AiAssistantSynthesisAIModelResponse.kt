73569d15f04165810f77cea2d1e98485039cebc9
Ready-Bridge <hanjungyo5533@gmail.com>
2025-05-06 15:14:55 +0900

    feat: AI 비서 다중 카테고리 음성 합성 관련 DTO 구현

--- backend/src/main/kotlin/kr/ac/kookmin/cs/capstone/voicepack_platform/aiAssistant/dto/response/synthesis/AiAssistantSynthesisAIModelResponse.kt
+++
@@ -1,9 +0,0 @@
-package kr.ac.kookmin.cs.capstone.voicepack_platform.aiAssistant.dto.response.synthesis
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class AiAssistantSynthesisAIModelResponse(
-    val audio_url: String,
-    val duration: Double
-)
